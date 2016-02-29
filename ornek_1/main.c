7a0fd2e6b29568989b57460bb05f4d522de64f8f
ecenurozturk <ecenurrozturk@gmail.com>
2016-02-29 23:56:38 +0200

    ilk ornek tamam

---
+++ ornek_1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char isim[10];
+
+    printf("isminizi giriniz = ");
+    scanf("%s",isim);
+
+    if (strcmp(isim, "ecenur") == 0)
+{
+    printf("hoşgeldiniz.nasılsınız?");
+}
+    else
+    {
+        printf("git burdan %d",isim);
+    }
+
+    return 0;
+}
