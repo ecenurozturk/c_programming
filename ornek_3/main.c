54b534ec2eab3b74dd75a25ddab46b3f2716e768
ecenurozturk <ecenurrozturk@gmail.com>
2016-03-02 22:25:59 +0200

    ornek_3 güncellendi

--- ornek_3/main.c
+++ ornek_3/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+int main(){
 
-int main()
-{   int sayi;
-
+    int sayi;
 
     printf("Bir Sayi Giriniz : \n");
     scanf("%d", &sayi);
 
-        if(sayi<0){
+    if(sayi<0)
+    {
+        sayi=sayi*-1;
+        printf("girdiginiz sayinin toplamaya gore tersi : %d\n",sayi);
+    }
+    else
+    {
+    	sayi=sayi*2;
+        printf("\n girdiginiz sayinin iki kati: %d\n", sayi);
+    }
 
-          sayi=sayi*-1;
-    printf("girdiginiz sayinin toplamaya gore tersi : %d\n",sayi);
-        }
-       else {
-          sayi=sayi*2;
-
-    printf("\n girdiginiz sayinin iki kati: %d\n", sayi);
-}
     return 0;
-}
+ }
