aff8e58d2ad3eec547bed2241e296ec253a15136
ecenurozturk <ecenurrozturk@gmail.com>
2016-03-02 21:49:18 +0200

    orne_3 tamam

---
+++ ornek_3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int sayi;
+
+
+    printf("Bir Sayi Giriniz : \n");
+    scanf("%d", &sayi);
+
+        if(sayi<0){
+
+          sayi=sayi*-1;
+    printf("girdiginiz sayinin toplamaya gore tersi : %d\n",sayi);
+        }
+       else {
+          sayi=sayi*2;
+
+    printf("\n girdiginiz sayinin iki kati: %d\n", sayi);
+}
+    return 0;
+}
