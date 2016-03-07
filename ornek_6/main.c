78e76420bc1a42de4cd63f2b88eba0bd64ec8559
ecenurozturk <ecenurrozturk@gmail.com>
2016-03-07 22:25:31 +0200

    ornek_6 tamam

---
+++ ornek_6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+main(){
+
+   int i;
+   char ascii;
+
+   printf("ascii gosterilecek karakteri giriniz :");
+   scanf("%c",&ascii);
+
+   printf("%d ",ascii);
+
+  return 0;
+}
