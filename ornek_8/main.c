8c53c7bc0c184f5612d5a0fe33d1782791ae05ff
ecenurozturk <ecenurrozturk@gmail.com>
2016-03-09 23:15:24 +0200

    ornek_8 tamam

---
+++ ornek_8/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+  int main()
+   {
+      int a, b, c, max;
+
+       printf("3 tane birbirinden farkli sayi yaziniz:");
+       scanf("%d %d %d", &a, &b, &c);
+
+       max = a;
+  if(b > max)
+       max = b;
+
+  if(c > max)
+       max = c;
+
+       printf("girilen sayilarin en buyugu = %d ", max);
+
+ return 0;
+   }
